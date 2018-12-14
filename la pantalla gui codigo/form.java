2dc6c07942ef75d6f07632b5f5ae903226d47dc2
mjavier1 <mjaguirre5@gmail.com>
2018-12-13 22:16:14 -0400

    primera codigo

---
+++ la pantalla gui codigo/form.java
@@ -0,0 +1,15 @@
+package designgui;
+import javax.swing.JFrame;
+public class form extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		form gui=new form();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setSize(200,200);
+		gui.setVisible(true);
+		gui.setTitle("Primera pantalla");
+		
+	}
+
+}
