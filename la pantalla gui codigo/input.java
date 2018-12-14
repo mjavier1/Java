3723a616aef004f8ec9f719ef6a7b06a7999fc75
mjavier1 <mjaguirre5@gmail.com>
2018-12-13 23:01:48 -0400

    Update input.java

--- la pantalla gui codigo/input.java
+++ la pantalla gui codigo/input.java
@@ -1,10 +1,19 @@
 package inputpane;
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane; //la libria para funciona dos texto
 
 public class input {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	/*Marcos aguirre
+	 * programa termite corre dos input despues sumar
+	 * https://www.youtube.com/watch?v=jJjg4JweJZU&index=2&list=PLcTTsr6vQDf3pW0MfF33rTOufHKG-8HZL
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+		
 		String fn=JOptionPane.showInputDialog("Enter first number");
 		String n=JOptionPane.showInputDialog("Enter first number");
 
