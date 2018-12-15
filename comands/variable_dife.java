08edd61425228b276ec216d3f3b60e249b029112
mjavier1 <mjaguirre5@gmail.com>
2018-12-14 21:49:07 -0400

    Add files via upload

---
+++ comands/variable_dife.java
@@ -0,0 +1,27 @@
+package variable;
+
+public class variable_dife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto="escribe lo quiera";
+				/*
+				 * marcos Aguirre
+				 * como declara la variable
+				 */
+		short x = 6;
+		  int y = 4;
+		 float a = 12.5f;
+		 float b = 7f;
+		
+		System.out.println("x is " + x + " , y is " + y);
+		  System.out.println("x + y = " + (x + y));
+		System.out.println("x - y = "  + (x - y));
+		 System.out.println("x / y = " + (x / y));
+		 System.out.println("x % y = " + (x % y));
+		 System.out.println("a is " + a + ", b is " + b);
+		  System.out.println("a / b = " + (a / b));
+		  System.out.println(texto);
+	}
+
+}
