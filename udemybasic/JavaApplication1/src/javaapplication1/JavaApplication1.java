97b675ea4bfa09374d03b3854b710d0148ba6872
mjavier1 <mjaguirre5@gmail.com>
2021-05-07 21:13:50 -0400

    variable

---
+++ udemybasic/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author mj
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //udemy eclise 
+        //Java Programming: Complete Beginner to Advanced
+        int value;
+        value=42;
+        short ashort =93;
+        long along =8345;
+        double dou=3.14;
+        float ft=3.3f;
+        char lett='c';
+        boolean bool=true;
+        byte fbyte=12;
+        
+                //print values
+       System.out.println(ashort);
+        System.out.println(value);
+          System.out.println(ashort);
+            System.out.println(along);
+              System.out.println(ft);
+                System.out.println(lett);
+          System.out.println(bool);
+            System.out.println(fbyte);
+        
+        //System.out.println("hello");
+    }
+    
+}
