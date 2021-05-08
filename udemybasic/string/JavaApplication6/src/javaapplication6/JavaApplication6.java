15e09fd1b7d8efc5399a317f55af7fbfd26dec11
mjavier1 <mjaguirre5@gmail.com>
2021-05-07 21:39:01 -0400

    Add files via upload

---
+++ udemybasic/string/JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author mj
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+        // TODO code application logic here
+        String mjl="HELLO";
+        int anumber =55;
+        System.out.print(mjl+" "+ " "+anumber);
+        
+    
+    
+    }
+    
+}
