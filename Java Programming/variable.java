43536251b0f77c0ab0a8907c99901c71a11eda92
mjavier1 <mjaguirre5@gmail.com>
2021-06-21 19:42:10 -0400

    varoable

---
+++ Java Programming/variable.java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main(String args[]) {
+     //variable int 
+      //sum variable
+
+      System.out.println("Hello world");
+      int number =5;
+      int num2=10;
+      int t;
+      t=num2+number;
+      System.out.println("sum is " +t);
+      
+    }
+}
