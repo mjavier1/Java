996aa74bf6e525c92fe02432d12850082ec48833
mjavier1 <mjaguirre5@gmail.com>
2021-06-22 18:26:32 -0400

    Create for.java

---
+++ Java Programming/for.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    public static void main(String args[]) {
+        //genrally we are using for loop
+        
+        for (int i=0; i<=12;i++){
+            
+            System.out.println("Value of i " + i);
+            //defore i *2 table
+        }
+        
+
+
+        
+    }
+    
+}
+    
