872dce5cac5b4f3c745cc058d259d2f43f12ab3a
mjavier1 <mjaguirre5@gmail.com>
2021-06-22 17:01:02 -0400

    Create if.java

---
+++ Java Programming/if.java
@@ -0,0 +1,30 @@
+public class MyClass {
+    public static void main(String args[]) {
+        //i learn declaration of variable
+      //if elseif else 
+      //condition with valor greater less equal sysmbols
+    int player=0;
+    int score_goal=1000;
+    player=100;
+    
+    if(player>=score_goal){
+        player=0;
+         System.out.println("you won the game");
+        player=0;
+    }
+    
+   else if(player <score_goal){
+     System.out.println("sorry try it again you lost the learn");
+        
+    }
+    else
+    {
+         System.out.println("you won the game");
+        player=0;
+        
+    }
+      
+
+      
+    }
+}
