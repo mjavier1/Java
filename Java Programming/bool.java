7d70d149cb2f793a8f5dd75af189e7abf3e3dcaa
mjavier1 <mjaguirre5@gmail.com>
2021-06-22 17:30:17 -0400

    Create bool.java

---
+++ Java Programming/bool.java
@@ -0,0 +1,39 @@
+/*
+
+public class MyClass {
+    public static void main(String args[]) {
+        //i learn declaration of variable
+
+boolean mybool=true;
+if(mybool=true){
+    System.out.println("my bolean is true");
+    
+    
+}
+    int a=5;
+    int b=10;
+    if(a<b){
+        System.out.println("a is smaller then b");
+    }
+
+      
+    }
+}
+example differential
+*/
+public class MyClass {
+    public static void main(String args[]) {
+        //i learn declaration of variable
+
+int player_score = 0;
+int score_goal=1000;
+boolean has_key=true;
+if(player_score > score_goal){
+    
+    System.out.println("this point of time you won this level");
+    
+}
+if(has_key)
+      System.out.println("you also has a key and u clear the area");
+    }
+}
