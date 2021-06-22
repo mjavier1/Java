50a235a5d4113d7afde3cbfcdf61124af63ec95f
mjavier1 <mjaguirre5@gmail.com>
2021-06-22 17:45:10 -0400

    while_java_do_while

---
+++ Java Programming/while.java
@@ -0,0 +1,22 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int number1 = 5;
+        int number2=10;
+
+/*
+while(number1 <=number2){
+ 
+ System.out.println("This is where i see loop"+number1);   
+ number1++;
+}    
+    
+    */
+    do{
+         System.out.println("This is where i see loop" +number1);   
+          number1++;
+    }
+    while(number1 <=number2);
+        
+    }
+    
+}
