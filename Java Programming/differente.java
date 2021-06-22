b4b22bd41766686dca8ed7592468ad7f5ba5a8e1
mjavier1 <mjaguirre5@gmail.com>
2021-06-21 20:02:45 -0400

    java_difference_variable

---
+++ Java Programming/differente.java
@@ -0,0 +1,43 @@
+public class hello {
+    public static void main(String args[]) {
+     
+/*
+data type
+permitive
+boolean
+numeric-char,integral
+integer-byte-short-int-long
+floating-point
+float-double
+non-primitive
+string
+array
+etc
+
+*/
+
+      System.out.println("Hello world");
+      float number =5;
+      float num2=10;
+      float  t;
+      t=num2/number;
+      System.out.println("sum is " +t);
+      byte my_byte =10;
+      int my_int=1000;
+      float my_float=2.5;
+    short my_short=100;
+    long mylon=10000;
+    double mydouble=3.14556;
+    String nombre="mjDevelop";
+    char charters='c';
+    //variable different
+    
+    
+    
+    
+    
+    
+    
+        
+    }
+}
