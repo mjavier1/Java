dde534e422c069d77bab39c4dcce296caaf8b649
mjavier1 <mjaguirre5@gmail.com>
2021-06-22 16:36:11 -0400

    comment single and block

---
+++ Java Programming/comment.java
@@ -0,0 +1,16 @@
+public class MyClass {
+    public static void main(String args[]) {
+        //i learn declaration of variable
+      int num1=10,num2=20,num3=30;
+      int y;
+      y=+num1+num2+num3;
+      
+
+      System.out.println("Sum of x+y = " + y);
+    
+        //this is  LINE comment
+        /*
+        block 
+        */
+    }
+}
