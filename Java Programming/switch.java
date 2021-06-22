67f0fc1a72ac96bdc0d4fac409e54873ab06f14d
mjavier1 <mjaguirre5@gmail.com>
2021-06-22 18:54:56 -0400

    Create switch.java

---
+++ Java Programming/switch.java
@@ -0,0 +1,24 @@
+
+public class MyClass {
+    public static void main(String args[]) {
+      int num=1;
+      
+      switch(num){
+          case 0:
+              System.out.println("This has the value 0 to it");
+           break;
+      case 5: case 6: case 7:
+           System.out.println("This has the value 5,6,7");
+          break;
+           case 10:
+           System.out.println("This has the value 5 to it");
+          break; 
+          
+          default:
+        System.out.println("This has the value no  to it");
+     
+      }
+      
+     
+    }
+}
