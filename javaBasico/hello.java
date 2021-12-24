690d130f5401730b9898036c393d345e178e1f48
mjavier1 <mjaguirre5@gmail.com>
2021-12-23 20:03:01 -0400

    proyecto
    salida comentario

---
+++ javaBasico/hello.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+      System.out.println("hello mundo basico java");
+		/*
+		basico comentario
+		*/
+            
+            //algo sencillo
+	}
+}
