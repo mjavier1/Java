5ebf7fc2894f7836586fa72cad08e568f84ab628
mjavier1 <mjaguirre5@gmail.com>
2021-12-24 16:50:31 -0400

    char
    letras de tesclado tambien suma 
    print sreen

---
+++ javaBasico/charter.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+    
+    /*
+    byte 8bits
+    int 32 bits
+    longs 
+    */
+	public static void main(String[] args) {
+	char c1,c2,c3;
+	c1=71;
+	c2='h';
+	c3=32;
+	System.out.println(c1+c2+c3);
+	System.out.println(""+c1+c2+c3);
+		System.out.printf("%c%c%c\n",c1,c2,c3);
+		c1++;
+		c2++;
+		c3++;
+		System.out.println("Despues de incremento");
+		System.out.println(c1+c2+c3);
+			System.out.println(""+c1+c2+c3);
+		System.out.printf("%c%c%c\n",c1,c2,c3);
+		
+	}
+}
