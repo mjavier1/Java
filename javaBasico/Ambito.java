b67112b78f47153ec39a6652f8fdb5f61cc943af
mjavier1 <mjaguirre5@gmail.com>
2021-12-29 11:29:43 -0400

    variable
    usando la funcion de raiz exponete

---
+++ javaBasico/Ambito.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int x=10;
+		if (x==10){
+		
+		    int y=20;
+		    System.out.println("x= " +x);
+		    System.out.println("y = " + y);
+		
+		} 
+	//	y=150; 
+	// no conconce una funct 
+		//inizalizancion
+		int a,b,c;
+		int d=3,e,f=4;
+		char x2='x';
+		byte z=22;
+		double pi =3.14159;
+		double g;
+		
+		a=4;
+		b=5;
+	//	g=Math.sqrt(a);
+	g=Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+		System.out.println("La hipotenusa es : " + g);
+		
+		
+		
+		
+	}
+}
