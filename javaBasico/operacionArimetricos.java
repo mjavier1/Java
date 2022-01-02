7861070b0bb2cb9c699f1596295ea6e72777f9bc
mjavier1 <mjaguirre5@gmail.com>
2022-01-02 14:38:18 -0400

    operacion
    variable falta terminal

---
+++ javaBasico/operacionArimetricos.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+
+//array for 
+{
+	public static void main(String[] args) {
+	
+	//Scanner sc= new Scanner(System.in);
+	//System.out.println("Ingrese el tiempo de analsis t =");
+	
+	System.out.println("Asignacion inefiente\n");
+	int a=5,b=7,c=4,d=6,e=10;
+	 a = a+3;
+	 b = b-9*a;
+	 c=c*6;
+	 d=d/3;
+	 e=e%2;
+	 System.out.println("a = "+a);
+	 	 System.out.println("b = "+b);
+	 	 	 System.out.println("c = "+c);
+	 System.out.println("d= "+d);
+	 	 	 System.out.println("e = "+e);
+
+
+        System.out.println("Asignacion eficiente");
+
+a = 5;
+
+	}
+}
