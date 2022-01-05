dddbc1af80e5752cf7fb42f7ca732a276174ee59
mjavier1 <mjaguirre5@gmail.com>
2022-01-05 18:53:42 -0400

    completo
    imcremento y decremento variable,
    tambien operaciones basica

--- javaBasico/operacionArimetricos.java
+++ javaBasico/operacionArimetricos.java
@@ -19,11 +19,25 @@ public class Main
 	
 	System.out.println("Asignacion inefiente\n");
 	int a=5,b=7,c=4,d=6,e=10;
-	 a = a+3;
-	 b = b-9*a;
-	 c=c*6;
-	 d=d/3;
-	 e=e%2;
+	// a = a+3;
+	// b = b-9*a;
+	// c=c*6;
+	 //d=d/3;
+	 //e=e%2;
+//incremento y decremento
+//b=5 + a++;
+//c=3 +b++;
+//d=7+e--;
+System.out.println("incremento y decremento como prefijos\n");
+a=5;b=7;c=4;d=6;e=10;
+//cuando el operador esta como prefijp primero se hace la operacion y despues se hace la asignacion
+a= --e+6;
+b= --d+3;
+e = --c+ a++;
+
+
+
+
 	 System.out.println("a = "+a);
 	 	 System.out.println("b = "+b);
 	 	 	 System.out.println("c = "+c);
@@ -32,8 +46,11 @@ public class Main
 
 
         System.out.println("Asignacion eficiente");
+//incremento y decremento prefijos subfijos
+//la expresion a=a+1; a+=1; es equivalente
 
-a = 5;
+
+
 
 	}
 }
