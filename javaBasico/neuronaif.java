fbe4a1b008192ee7bce7ba37daa14593ec9fe1a9
mjavier1 <mjaguirre5@gmail.com>
2022-01-06 12:24:15 -0400

    falta por termanal
    if basico

---
+++ javaBasico/neuronaif.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		int x1,x2;
+		System.out.println("Ingrese un valor logico (1,0) :");
+		x1=sc.nextInt();
+	    System.out.println("Ingrese un valor logico (1,0) :");
+		x2=sc.nextInt();
+		double w1=0.5,w2=0.5;
+		double b=-0.6;
+		boolean resultado=true;
+		System.out.println("Que operacion desea realizar"
+		
+		+"AND-bool, and neurona, 3 or bool");
+		int opcion;
+		opcion=sc.nextInt();
+		if (){
+		    
+		} else if(){
+		    
+		}else if(){
+		    
+		}else{
+		    
+		}
+		
+		
+	}
+}
