ce60d88f4408f8ab8c83ea42b1b809c7f7ab174d
mjavier1 <mjaguirre5@gmail.com>
2022-01-07 16:54:57 -0400

    loopwhile
    sum basico

---
+++ javaBasico/whileloop_dowhile.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+    //loop while basico 
+    //do while
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int numero=0, limite=5;
+		do{
+		    
+		    System.out.println("do-while numero  "+ numero);
+		  
+		    
+		}  while(numero++<limite);
+		numero=0;
+		while(numero++<limite){
+		    
+		    System.out.println("while numero = "+ numero);
+		}
+	}
+}
