3067892119e40bd3114ce0efccf1f71a0a0dfa4d
mjavier1 <mjaguirre5@gmail.com>
2022-01-17 15:27:29 -0400

    matriz
    (+ - / *)
    de matariz operacion matematica

--- javaBasico/matriz.java
+++ javaBasico/matriz.java
@@ -8,7 +8,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 import java.util.Scanner;
 
-class matriz{
+class Matriz{
     int n;
 		double m[][];
 		void capture(){
@@ -18,7 +18,7 @@ class matriz{
 		   m=new double[n][n];
 		   for(int i=0;i<n;i++){
 		       for(int j=0;j<n;j++){
-		   System.out.println("Ingrese el elemento de la material" + i+""+j +"):");
+		   System.out.println("Ingrese el elemento de la material " + i+" "+j + "):");
 		  m[i][j]=sc.nextDouble();
 		 
 		   }
@@ -36,7 +36,8 @@ class matriz{
 	    System.out.println();
 	    }  
 	}	
-        void SumarMatrizes(Matriz matriz1, Matriz matriz2){
+	//sumarMatrices(Matriz matriz1,Matriz matriz2) 
+   void sumarMatrices(Matriz matriz1,Matriz matriz2){
         if(matriz1.n == matriz2.n){
            n=matriz1.n;
            m= new double[n][n];
@@ -47,12 +48,84 @@ class matriz{
               
            }
         }
+        else {
+            System.out.println("\nLas matric0es no tienen la misma  dimension");
+      
+        }
         
         System.out.println("\n Resultado de la suma de matriz");
+        } 
+
+
+
+void restamartiz(Matriz matriz1,Matriz matriz2){
+        if(matriz1.n == matriz2.n){
+           n=matriz1.n;
+           m= new double[n][n];
+           for(int i=0;i<n;i++)
+           for(int j=0;j<n;j++){
+              m[i][j]= matriz1.m[i][j]-matriz2.m[i][j];
+              
+              
+           }
         }
-        else{
+        else {
             System.out.println("\nLas matric0es no tienen la misma  dimension");
+      
         }
+        
+        System.out.println("\n Resultado de la resta de matriz");
+        } 
+
+
+
+
+void multiamartiz(Matriz matriz1,Matriz matriz2){
+        if(matriz1.n == matriz2.n){
+           n=matriz1.n;
+           m= new double[n][n];
+           for(int i=0;i<n;i++)
+           for(int j=0;j<n;j++){
+              m[i][j]= matriz1.m[i][j]*matriz2.m[i][j];
+              
+              
+           }
+        }
+        else {
+            System.out.println("\nLas matric0es no tienen la misma  dimension");
+      
+        }
+        
+        System.out.println("\n Resultado de la multiplicar  de matriz");
+        } 
+
+
+
+void diviamartiz(Matriz matriz1,Matriz matriz2){
+        if(matriz1.n == matriz2.n){
+           n=matriz1.n;
+           m= new double[n][n];
+           for(int i=0;i<n;i++)
+           for(int j=0;j<n;j++){
+              m[i][j]= matriz1.m[i][j]/matriz2.m[i][j];
+              
+              
+           }
+        }
+        else {
+            System.out.println("\nLas matric0es no tienen la misma  dimension");
+      
+        }
+        
+        System.out.println("\n Resultado de la division de matriz");
+        } 
+
+
+
+
+
+
+
 
     
 } 
@@ -61,14 +134,29 @@ public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		matriz m1=new   matriz();
-		matriz m2=new matriz();
+		Matriz m1=new   Matriz();
+		Matriz m2=new Matriz();
 		m1.capture();
 		m2.capture();
 		
 m1.mostrarMatriz();
 m2.mostrarMatriz();
-matriz m3=
+Matriz m3= new Matriz();
+m3.sumarMatrices(m1, m2);
+m3.mostrarMatriz();
+m3.restamartiz(m1, m2);
+m3.mostrarMatriz();
+m3.multiamartiz(m1,m2);
+m3.mostrarMatriz();
+m3.diviamartiz(m1, m2);
+
+m3.mostrarMatriz();
+				
+	
+	
+	
+	
+	
 	
 	
 	    
