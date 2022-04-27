508b27fb838ed58b894bd98f50579b15daa97190
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-26 21:16:33 -0400

    tipo de datos
    variable la diferentes

---
+++ javaFacil/Tipodedatos.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+     //  byte mivaraible= 125;
+       //int , short , long , byte,
+       // System.out.println("" + mivaraible);
+    
+       
+     //  char micatera = 'A';
+     String nombre="Mj";
+       int numero=0;
+       double pro=5.7;
+       //double pro=(double)mi_pro;
+       int mi_pro = (int)pro;
+       
+       
+       
+       long miviable=9223372036854775807L;
+          float  promdeio = 10.6f;
+          boolean bandera = false;
+          //bandera = 3==3 t
+          
+        System.out.println(""+miviable);
+        System.out.println("Valor es float "+promdeio); 
+        System.out.println(""+bandera);
+        System.out.println(" "+nombre);
+        System.out.println(" "+mi_pro);
+    
+    }
+}
