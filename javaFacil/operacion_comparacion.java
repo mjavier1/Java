c3b73040a673ff35eb3869b9b6d4dbd66c478aec
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-26 22:22:48 -0400

    Create operacion_comparacion.java

---
+++ javaFacil/operacion_comparacion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    
+      int edadJuan=23;
+      int edadSamel=18;
+      int edadsara=32;
+      int edadAngela=23;
+      
+      int rest;
+      boolean Soniguales
+        System.out.println("");
+      
+        
+    // -, + , / ,*
+    }
+}
