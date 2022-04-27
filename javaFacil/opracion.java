70d94fda7e0206a23634bf851905d46f60a78bb8
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-26 21:59:04 -0400

    operaccion java
    java operacion matematica

---
+++ javaFacil/opracion.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    
+        int salario1=5000;
+        int salarios = salario1 + salario1;
+        int a=100;
+        int b=7;
+        salario1++;
+        int i=0;
+        int j=0;
+        j= i++;
+        int aa=5;
+        aa=aa+5;
+        
+        
+        
+        
+        int reslt= a%b;
+        
+        System.out.println("j");
+        System.out.println(" "+reslt);
+        System.out.println("Variable salarios " + salarios);
+        System.out.println("int aa" + aa);
+        
+    // -, + , / ,*
+    }
+}
