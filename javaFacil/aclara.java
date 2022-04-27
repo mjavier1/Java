2ab347cc76cebdafb22a86935b0241a260a2cfbc
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-26 21:57:13 -0400

    Create aclara.java

---
+++ javaFacil/aclara.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    
+        int a=(5*6)/2;
+        System.out.println(a);
+        
+    // -, + , / ,*
+    }
+}
