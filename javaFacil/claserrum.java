7464a10c733f95c5449631db38b340e0faef4068
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 15:48:09 -0400

    obj
    class obj new differetial parts funt

---
+++ javaFacil/claserrum.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class clasersum {
+    
+    public  int sumadosenteros(int a, int b){
+        
+            int restultados=0;
+            restultados = a+b;
+            
+            
+            
+        return restultados;
+        
+        }
+    
+}
