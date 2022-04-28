fcfac774432cad8d005c1369776216bdf8d969fd
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 15:46:35 -0400

    metodos class
    class metodos 
    funciones

---
+++ javaFacil/metodos.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    //main metodos wxcutados programas
+    int numero1=45;
+    int numero3=80;
+    int num3=24;
+    clasersum obj = new clasersum();
+    
+    
+    //sumadosenteros(numero1,numero3);
+    int sumarreslt=  obj.sumadosenteros(numero1,numero3);
+    int sumarreslt1 = obj. sumadosenteros(numero1,num3);
+    
+        System.out.println(sumarreslt);
+         System.out.println(sumarreslt1);
+    
+    }
+        
+    }
+
