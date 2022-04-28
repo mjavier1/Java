9885ef658b1ad7885354c89607280f491da802ea
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 16:01:30 -0400

    void stativ

---
+++ javaFacil/void.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    //main metodos wxcutados programas
+    int numero1=45;
+    int numero3=80;
+    int num3=24;
+    
+    sumadosenteros(numero1,numero3);
+ 
+    
+      
+    sumadosenteros(numero3,num3);
+         
+         
+    }
+        public static void  sumadosenteros(int a, int b){
+        
+            //boolean  restultados=true;
+            
+          int  restultados = a+b;
+            
+            System.out.println(restultados);
+            
+     //   return restultados;
+        
+        }
+    }
+
