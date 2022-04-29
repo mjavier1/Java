6c1ffd8d738c425383fddb01e4734e5e2b11262e
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-29 16:13:42 -0400

    for loop

---
+++ javaFacil/for.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+
+        
+        for (int i = 0; i < 7; i++) {
+            
+            for (int j = 0; j < 5; j--) {
+                
+                System.out.println("la variable j se ha impreso :"+j+"veces");
+            }
+            
+          //  System.out.println("La variable i se ha impre: "+i);
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+}
