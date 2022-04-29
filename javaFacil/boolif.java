eba678eefc870364a5545888e6f8f69774676037
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 21:33:50 -0400

    booljava

---
+++ javaFacil/boolif.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    
+        int edadjuan=23;
+        int edadsa=18;
+        int edadsara=32;
+        int edadangela=23;
+        
+        boolean flag=true;
+        //verdadera && falso 
+        //comparacion 
+        
+        //flag==true
+        //
+        
+        if (!flag==false) {
+            System.out.println("La bandera es verdadera");
+            
+        }else{
+        
+            System.out.println("La bandera es falsa");
+        }
+        
+        
+        
+    }
+     
+    
+}
+
