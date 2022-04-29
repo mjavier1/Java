561747dadef47aecb8d5d3b45518a8c26bccaba5
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 22:52:28 -0400

    case
    switch if

---
+++ javaFacil/switch.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    
+   
+       
+        
+        
+        String name = "mj";
+       int a=0;
+       
+       switch (name) {    
+      
+                
+            case "m": 
+                a=5;
+                System.out.println("hello");
+                
+                break;
+                case "mj":
+                    System.out.println("el caso de mj fue activado");
+                break;
+            default:
+       
+        
+               System.out.println("el caso de fabrica fue  activado");
+        }
+       
+        
+        //else{
+          // System.out.println("Sara es mayor samuel");
+          // System.out.println("La bandera es falsa");
+        //}
+        
+         System.out.println(a);
+        
+    }
+    
+}
+    
+
+
