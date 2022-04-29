b4a5e48ffc94434514dc6ee9906f2e0e1fff3951
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 22:04:49 -0400

    bool

---
+++ javaFacil/boolfalsa.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    
+        int edadjuan=23;
+        int edadsa=18;
+        int edadsara=32;
+        int edadangela=23;
+        
+        boolean flag=false;
+       boolean flag1=false;
+       
+        if (flag || flag1) {
+            System.out.println("true");
+            
+        }
+        else{
+            System.out.println("falsa");
+        }
+       
+       
+        
+        //verdadera && falso 
+        //comparacion 
+        
+        //flag==true
+        //
+        
+       
+        }
+        
+        
+        
+        
+        
+        
+        //else{
+          // System.out.println("Sara es mayor samuel");
+          // System.out.println("La bandera es falsa");
+        //}
+        
+        
+        
+    }
+     
+    
+}
+
