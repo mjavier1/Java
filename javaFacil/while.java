51deeccba749f6485d341f5287ca538276c3beeb
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 23:07:05 -0400

    loop

---
+++ javaFacil/while.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    String name="mj";
+    
+   
+       int i=0;
+    
+       
+       while(i<5){ 
+           i++;
+            System.out.println("El nombre es ha impreso "+name+" "+i);
+           //sola 
+            //break;
+       }
+       
+       
+       
+       
+       
+       
+      // while (i<10) {            
+        //    i=i+1;
+          //  System.out.println("El nombre es ha impreso "+name+" "+i);
+       
+      // }
+        
+        
+               
+    }
+    
+}
+    
+
+
