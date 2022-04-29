e6a47078671bcde65cac207cecfcfb73fd81e371
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 21:53:18 -0400

    ifjava

---
+++ javaFacil/ifif.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    
+        int edadjuan=23;
+        int edadsa=18;
+        int edadsara=32;
+        int edadangela=23;
+        
+        boolean flag=true;
+        //verdadera && falso 
+        //comparacion 
+        
+        //flag==true
+        //
+        
+        if (edadsara < edadsa) {
+            System.out.println("Sara es menor samuel");
+           
+///System.out.println("La bandera es verdadera");
+            
+        }
+        if (edadsara >= edadsa) {
+            System.out.println("Sara es mayor samuel");
+           
+///System.out.println("La bandera es verdadera");
+            
+        }
+        
+        
+        
+        
+        
+        
+        //else{
+          // System.out.println("Sara es mayor samuel");
+          // System.out.println("La bandera es falsa");
+        //}
+        
+        
+        
+    }
+     
+    
+}
+
