addcaff6b1608e552cc6a2107d3a5e7ba936f1a8
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-28 20:16:37 -0400

    Create array.java

---
+++ javaFacil/array.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+    //main metodos wxcutados programas
+int a=1;
+int b=2;
+int c=3;
+int d=5;
+int e=6;
+int num[]= {1,2,3,4,5,6,7,8};
+
+        //new int [5];
+
+//num[3]=4;
+
+//num = new int [5]
+
+int ultimo=num[num.length-1];
+
+
+//aaray
+         
+        System.out.println("El arreglo en el indice "+num[4]);
+        System.out.println("El ultimo elemento de arreglos es "+ultimo);
+    }
+     
+    
+}
+
