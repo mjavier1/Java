96268108bcd4423955a8cb79405670b40c9f9813
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-04-29 15:56:07 -0400

    do while
    loop

---
+++ javaFacil/dowhile.java
@@ -0,0 +1,51 @@
+
+package com.mycompany.tipodedatos;
+
+/**
+ *
+ * @author mj
+ */
+public class Tipodedatos {
+
+    public static void main(String[] args) {
+
+        
+        int a=10;
+        String name = "samuel";
+        int i=0;
+        
+        
+        do {            
+            
+            System.out.println("Me imprimi antes de la condicion"+i);
+            i++;
+            
+        } while (i<10);
+        
+        //loop math
+        
+        
+        
+        
+        
+        
+        /*
+        String name= "samuel";
+        int i=0;
+        while(i<5)
+        {
+         
+            System.out.println("El nombre "+ name );
+        
+        }
+            
+            */
+  
+        
+        
+        
+        
+        
+        
+    }
+}
