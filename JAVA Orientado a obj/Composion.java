ecd6c222d104ac43abe9ea4512a23acafa02e518
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-05-01 19:50:19 -0400

    racional

---
+++ JAVA Orientado a obj/Composion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package composion;
+
+/**
+ *
+ * @author mj
+ */
+public class Composion {
+
+    /**
+     * @param args the command line arguments
+     * composicion 
+     * Que es Es el mecanismo en el cual una clase se construye a patrir de otros objetos 
+     * de igual o distinto tipo, pudiendolos combinar para obtener la funcionalidad deseada.
+     * 
+     * 
+     * en la composicion la nueva clas, mantiene una relacion una tiene un uses has al con los objetos que son partes de la clase.
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
