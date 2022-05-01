7fdd68992a658f4df15eeb2da17b108de220ba7d
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-05-01 16:19:54 -0400

    componetes

---
+++ JAVA Orientado a obj/componetes.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udemy.poo.codigo;
+
+/**
+ *
+ * @author mj
+ */
+public class componetes {
+    private float tiempo;
+    private float x;
+    private float y;
+
+    public componetes(float tiempo, float x, float y) {
+        this.tiempo = tiempo;
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(float tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    
+    
+}
