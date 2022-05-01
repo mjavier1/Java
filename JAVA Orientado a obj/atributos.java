768fb8cc265150849674ab78ae718dd80a69dd94
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-05-01 13:30:57 -0400

    java ciclosAtributos
    informancion

---
+++ JAVA Orientado a obj/atributos.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package udemy.poo.inicio;
+
+/**
+ *
+ * @author mj
+ */
+
+
+// La programancion orientada a objetos se define como un paradigma que permite realizar
+//una abstracion de la realidad, que se puede implementar en una aplicacion de software con el fin de resolver problemas 
+//mediante el uso de un lenguaje de programancion 
+
+
+//paquete 
+
+/*
+Es un contenedor de clases. se realizar para ordenar el codigo de forma consistente de acuerdo a los servicios implementados
+*/
+//Clase 
+// se define como un tipo abstracto de dato que contiene atributos y metodos. a traves de una clase se implementa un cocencto abstraido de la realidad.
+//Objetos
+//Es la referencia e instancia de una clase. al crea una referencia se asigna un espacio de memoria dinamica al objeto, pero no es utilizable.
+
+//Atributos : hacen referencia a las caracteriscas que se le le incluyen a la clase
+//visibilidad se refiere al nivel de accesibilidad de los atributos y metodos
+
+//a> private se puede acceder desde un metodo implementado desde la misma clase.
+// public se puede acceder desde un metodo implementado en cualquier clase
+//Protected se puede acceder desde un metodo implementodo en una clase que herede la clase que contiene esta visibilidad y desde clases implementadas en el mismo paquetes
+
+/*
+Hacen referencia a los servicio que se le incluyen a la clase. Un metodo contiene los siguientes elementos:
+1 visibilidad . se debe establecer si el metodo es private, public o protectd.
+2.Retorno Un metodo puede retornar informancion. si el metodo no retorna informancion se debe colcar la palabra reservada "void"
+3. Nombre identificador del metodo en la clase.
+4.parametros un parametros puede ser un tipo primitivo de dato o una declaracion de una clase. los 
+parametros deben estar separados por comas
+
+Encapsulamiento 
+Es una caracteristica que indica que los atributos que definen propiedades propias de la clase deben tener visibilidad private. De esta forma
+se ofrece seguridad a la informancion depositada en dicho atributos.
+
+sentecial final
+una clase puede tener atributos finales que hacen referencia a cosnstantes que no pueden cambiar su valor en tiempo de ejecucion de la aplicacion
+el apuntador this permite acceder a los atributos y metodos de la clase
+
+
+
+
+*/
+
+
+
+public class ciclosAtributos {
+    
+    
+}
