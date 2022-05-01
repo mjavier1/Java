7f50241893f0c046dfe4bf0aa6007075b5d9c08e
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-05-01 16:19:01 -0400

    inicio class

--- JAVA Orientado a obj/inicioClass.java
+++ JAVA Orientado a obj/inicioClass.java
@@ -5,6 +5,7 @@
 package udemy.poo.codigo;
 
 import javax.swing.JOptionPane;
+ 
 
 /**
  *
@@ -18,8 +19,23 @@ public class inicio {
      String datos2 =   JOptionPane.showInputDialog(null,"Dame numero de velocidad inicial ");
         int numdos=Integer.parseInt(datos2);
            
-        TipoParabolicos obj = new TipoParabolicos(numUno,numdos);
-        JOptionPane.showMessageDialog(null, "La altura maxima es : " + obj.alturaMaxima()+ "El alcance es :" + obj.alcance(),"Resultado ", JOptionPane.INFORMATION_MESSAGE);
+        TipoParabolicos obj1 = new TipoParabolicos(numUno,numdos);
+        JOptionPane.showMessageDialog(null, "La altura maxima es : " + obj1.alturaMaxima()+ "El alcance es :" + obj1.alcance(),"Resultado", JOptionPane.INFORMATION_MESSAGE);
+        
+        //componetes 
+        String datos3 =   JOptionPane.showInputDialog(null,"Dame el tiempo ");
+        int numt=Integer.parseInt(datos3); 
+      
+       String y = null;
+      
+       componetes[]datoss=obj1.calcComp(numt);  
+       
+    
+        for (componetes objeto:datoss) {
+            y=objeto.getTiempo() +" ; "+ objeto.getX()+" ; " + objeto.getY()+"\n";
+            
+        }
+        JOptionPane.showMessageDialog(null, y,"compontes,",JOptionPane.INFORMATION_MESSAGE);
         
         
         
